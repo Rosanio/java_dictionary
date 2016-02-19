1c9a050f3cbeaaf1181e4e39a000ad0368052abc
Matt Rosanio <mmrosani@syr.edu>
2016-02-19 08:05:47 -0800

    Add failing test to initialize new instance of Word class

--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 public class WordTest {
-  
+
+  @Test
+  public void word_initializesCorrectly() {
+    Word newWord = new Word("Peanuts");
+    assertEquals(true, newWord instanceof Word);
+  }
 }
