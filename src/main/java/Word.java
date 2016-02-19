5992d2f5873d4394c8edb98633babea8b603a5a3
Matt Rosanio <mmrosani@syr.edu>
2016-02-19 08:16:58 -0800

    Add all method to Word class to return an ArrayList of all created instances of Word

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,13 +1,20 @@
 import java.util.*;
 
 public class Word {
+  private static ArrayList<Word> words = new ArrayList<Word>();
+
   private String mWord;
 
   public Word(String word) {
     mWord = word;
+    words.add(this);
   }
 
   public String getWord() {
     return mWord;
+  }
+
+  public static ArrayList<Word> all() {
+    return words;
   }
 }
