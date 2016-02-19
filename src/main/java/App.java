55c5ee01d1013ddeeac37b9b188b2142402ff90b
Matt Rosanio <mmrosani@syr.edu>
2016-02-19 09:37:59 -0800

    Setup landing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -7,6 +7,12 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
